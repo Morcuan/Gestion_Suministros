81d1deb5a3e4e8a03218c3cde0f9fb3a37ec2daa
Antonio <antonio@pepino.local>
2026-06-02 18:47:00 +0200

    Nivelaccion manual: Nivelacion manual reinstalacion vscode. 2026-06-02 18:47

--- nivelacion.sh
+++ nivelacion.sh
@@ -77,7 +77,7 @@ fi
 
 # 2) Commit si hay algo en staging
 if [ $HAY_CAMBIOS_STAGING -eq 1 ]; then
-    COMMIT_MSG="Nivelaccion manual: Preparacion para .deb incluyendo el db_init.py final. $(date '+%Y-%m-%d %H:%M')"
+    COMMIT_MSG="Nivelaccion manual: Nivelacion manual reinstalacion vscode. $(date '+%Y-%m-%d %H:%M')"
     echo "💾 Realizando commit: $COMMIT_MSG"
     git commit -m "$COMMIT_MSG" || {
         echo "❌ Error realizando el commit."
